5bf7e9efba02835638e8b65e3d8175f086d8d2db
AR-20 <atul@gottdata.com>
2024-08-18 17:18:45 +0530

    Commiting final version

--- ScrapeGetYourGuide.py
+++ ScrapeGetYourGuide.py
@@ -6,6 +6,11 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+# Define a global variable to determine if application is in development more or not
+DEVELOPMENT_MODE = True
 
 
 def get_firefox_driver(opts):
@@ -17,12 +22,13 @@ def get_firefox_driver(opts):
 
 
 def get_driver(opts):
-    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=opts)
+    return webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()), options=opts
+    )
 
 
 OPTIONS = Options()
-OPTIONS.add_argument('--disable-gpu')
-OPTIONS.add_argument('--headless')
+OPTIONS.add_argument("--disable-gpu")
 
 FIREFOX_OPTIONS = FirefoxOptions()
 FIREFOX_OPTIONS.add_argument("--disable-gpu")
@@ -31,29 +37,71 @@ FIREFOX_OPTIONS.add_argument("--headless")
 
 class ScrapeGetYourGuide:
     def __init__(self):
-        self.driver = get_firefox_driver(FIREFOX_OPTIONS)
+        if DEVELOPMENT_MODE:
+            self.driver = get_driver(OPTIONS)
+        else:
+            self.driver = get_firefox_driver(FIREFOX_OPTIONS)
+
+        self.driver.implicitly_wait(5)
+
+    def scrape_one_trip_info(self, url: str) -> str:
+        # Go to the webpage
+        self.driver.get(url)
+
+        # self.driver.implicitly_wait(10)
+        res = {}
+
+        # Get highlights
+        try:
+            trip_highlights = self.driver.find_element(
+                By.CSS_SELECTOR, 'ul[data-test-id="activity-highlights"]'
+            )
+            li_elements = trip_highlights.find_elements(
+                By.CSS_SELECTOR, "li.activity-highlights__list-item"
+            )
+
+            highlights = []
+            for element in li_elements:
+                highlights.append(element.text)
+
+            highlight_string = "\n".join(highlights)
+            res["highlights"] = highlight_string
+        except Exception as e:
+            print("There was an exception while getting the highlights: ", e)
+            res["highlights"] = "None found"
+
+        return res["highlights"]
 
     def get_response(self, url: str) -> list:
         # Got to the webpage
         self.driver.get(url)
         print("Going to url: ", url)
 
-        self.driver.implicitly_wait(10)
+        # self.driver.implicitly_wait(10)
 
         # Find all the trips
         try:
-            trips = self.driver.find_elements(By.CSS_SELECTOR, ".vertical-activity-card")
+            trips = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_all_elements_located(
+                    (By.CSS_SELECTOR, ".vertical-activity-card")
+                )
+            )
         except Exception as e:
             print("The following exception occured: ", e)
             return [{"message": "Could not find any trips", "status": 400}]
 
+        status = 0
+
         if len(trips) != 0:
+            status = 200
             res = []
             for trip in trips:
                 single_trip = {}
                 # Get the title of the trip
                 try:
-                    title = trip.find_element(By.CSS_SELECTOR, 'p[data-test-id="activity-card-title"]')
+                    title = trip.find_element(
+                        By.CSS_SELECTOR, 'p[data-test-id="activity-card-title"]'
+                    )
                     single_trip["title"] = title.text
                 except Exception as e:
                     print("The following exception occured: ", e)
@@ -61,7 +109,9 @@ class ScrapeGetYourGuide:
 
                 # Get the rating of the trip
                 try:
-                    rating = trip.find_element(By.CSS_SELECTOR, 'span.rating-overall__rating-number')
+                    rating = trip.find_element(
+                        By.CSS_SELECTOR, "span.rating-overall__rating-number"
+                    )
                     single_trip["rating"] = rating.text
                 except Exception as e:
                     print("The following exception occured: ", e)
@@ -69,7 +119,9 @@ class ScrapeGetYourGuide:
 
                 # Get the review of the trip
                 try:
-                    reviews = trip.find_element(By.CSS_SELECTOR, 'div.rating-overall__reviews span')
+                    reviews = trip.find_element(
+                        By.CSS_SELECTOR, "div.rating-overall__reviews span"
+                    )
                     single_trip["num_reviews"] = reviews.text
                 except Exception as e:
                     print("The following exception occured: ", e)
@@ -77,9 +129,15 @@ class ScrapeGetYourGuide:
 
                 # Get the price of the trip
                 try:
-                    pricing_container = trip.find_element(By.CSS_SELECTOR, "div.baseline-pricing__container")
-                    pricing_value = pricing_container.find_element(By.CSS_SELECTOR, "div.baseline-pricing__value")
-                    pricing_category = pricing_container.find_element(By.CSS_SELECTOR, 'p.baseline-pricing__category')
+                    pricing_container = trip.find_element(
+                        By.CSS_SELECTOR, "div.baseline-pricing__container"
+                    )
+                    pricing_value = pricing_container.find_element(
+                        By.CSS_SELECTOR, "div.baseline-pricing__value"
+                    )
+                    pricing_category = pricing_container.find_element(
+                        By.CSS_SELECTOR, "p.baseline-pricing__category"
+                    )
 
                     single_trip["pricing_value"] = pricing_value.text
                     single_trip["pricing_category"] = pricing_category.text
@@ -90,7 +148,9 @@ class ScrapeGetYourGuide:
 
                 # Get the href of the trip
                 try:
-                    trip_href = trip.find_element(By.CSS_SELECTOR, "a.vertical-activity-card__container")
+                    trip_href = trip.find_element(
+                        By.CSS_SELECTOR, "a.vertical-activity-card__container"
+                    )
                     href_value = trip_href.get_attribute("href")
                     single_trip["link"] = href_value
                 except Exception as e:
@@ -99,13 +159,25 @@ class ScrapeGetYourGuide:
 
                 # Add in success message
                 single_trip["message"] = "Success!"
-                single_trip["status"] = 200
+                single_trip["status"] = status
 
                 # Append to result
                 res.append(single_trip)
         else:
+            status = 400
             print("No trips were found")
-            return [{"status": 400, "message": "Could not find any trips"}]
+            return [{"status": status, "message": "Could not find any trips"}]
+
+        # if status == 200:
+        #     for trip in tqdm(res):
+        #         if trip["link"] != "Not found":
+        #             try:
+        #                 self.driver.implicitly_wait(2)
+        #                 highlights = self.scrape_one_trip_info(trip["link"])
+        #                 trip["highlights"] = highlights
+        #             except Exception as e:
+        #                 print("An exception occurred executing this function: ", e)
+        #                 trip["highlights"] = "Not found"
 
         return res
 
@@ -124,4 +196,3 @@ def get_getyourguide_trips(links: list) -> list:
         res.extend(trips)
 
     return res
-
--- main.py
+++ main.py
@@ -1,18 +1,78 @@
+import configparser
+import os
+from agents.destination_info import get_destination_data
+from streamlit_chat import message
+
+from helpers.helpers import (
+    find_question_type,
+    answer_non_travel_question,
+    get_destination,
+    get_location_curiosity
+)
 import streamlit as st
-from ScrapeGetYourGuide import  get_getyourguide_trips
-from ScrapeGoogle import get_getyourguide_link
-import json
 
-search_query = st.text_input("Enter your search query (Has to be fro getyourguide at the moment")
+# Set up API Keys and Configs
+config = configparser.ConfigParser()
+config.read("config.ini")
 
-get_started_button = st.button("Get Started!")
+OPENAI_API_KEY = config.get("API", "OPENAI_API_KEY")
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
-if get_started_button:
-    web_links = get_getyourguide_link(search_query)
-    st.write("The links found from the web are: ", web_links)
-    st.write("getting info ...")
+# Configure streamlit
+if "history" not in st.session_state:
+    st.session_state.history = []
 
-    getyourguide_trips = get_getyourguide_trips(web_links)
-    output = json.dumps(getyourguide_trips, indent=5)
-    st.write("Showing scraped trips: ")
-    st.code(output, language='json')
+
+# Make a function to take in a question and answer:
+def generate_answer(question: str) -> str:
+    # FInd out if it is a travel question or note
+    is_travel_question = find_question_type(question)
+
+    ans = ""
+    if not is_travel_question:
+        return answer_non_travel_question(question)
+    else:
+        travel_question_answer = get_destination(question)
+        if "false__no_destination" in travel_question_answer:
+            ans = answer_non_travel_question(question)
+            return ans
+        else:
+            destination = travel_question_answer
+            is_curious = get_location_curiosity(destination=destination, question=question)
+            if is_curious:
+                ans = answer_non_travel_question(question)
+                return ans
+            ans = get_destination_data(
+                destination=destination, question=question
+            )
+            return ans
+
+
+def get_chatbot_reply():
+    user_message = st.session_state.input_text
+    result = generate_answer(user_message)
+
+    st.session_state.history.append({"message": user_message,
+                                     "is_user": True})
+    st.session_state.history.append({"message": result,
+                                     "is_user": False})
+
+
+# Streamlit application
+st.header("Chatbot for your Next Adventure")
+
+st.markdown("""
+    ##### Ask me about anything!
+    I work best with travel related questions, and I can find you a tour with real time data!
+""")
+
+prompt = st.text_input(placeholder="Enter your message here!",
+                       label="Talk to the bot:",
+                       key="input_text",
+                       on_change=get_chatbot_reply)
+
+for i, chat in enumerate(st.session_state.history):
+    message(**chat, key=str(i))
+
+
+
